a9f83bf563609ae6701764fa6b1c147ce39413de
smhamidi <1380hamidi@gmail.com>
2023-04-14 02:24:30 +0330

    scraing ganjoor.net up to layer 3

--- src/scraper.py
+++ src/scraper.py
@@ -1,22 +1,39 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
 
 class Scraper:
-    def __init__(self,):
-        pass
-    
-    @staticmethod
-    def get_poets_name(url: str)-> list:
-        if url == "http://epub.ganjoor.net/":
-            page = urlopen(url)
-            html_bytes = page.read()
-            html = html_bytes.decode("utf-8")
-            html_list = list(html.split("\n"))
-            html_list = [href for href in html_list if ("i.ganjoor" in href)]
-            links = []
-            for line in html_list:
-                splited_line = list(line.split('"'))
-                links.append([link for link in splited_line if "http" in link][0])
-            names = [name.split("/")[-1].split(".")[0].lower() for name in links]
-            return names
-                                
+    def __init__(self, poet_names=None, url=None):
+        self.poet_names = poet_names
+        self.url = url
         
+    
+    def set_poets_name(self, url: str)-> list:
+        if url == "https://ganjoor.net/":
+            self.url = url
+            html_text = requests.get(url).text
+            html_soup = BeautifulSoup(html_text,"lxml")
+            divs_list = html_soup.find_all("div", class_="caption")
+            addresses = [str(div.find("a")) for div in divs_list]
+            poet_names = [address.split('"')[1][1:] for address in addresses]
+            poet_names = list(set(poet_names))
+            
+            self.poet_names = poet_names
+        else:
+            raise ValueError("This program is not defined for scraping the provided URL.")
+            
+    @staticmethod
+    def check_url(url: str)-> bool:
+        try:
+            get = requests.get(url)
+            # if the request succeeds 
+            if get.status_code == 200:
+                return True
+            else:
+                return False
+        #Exception
+        except:
+            return False
+    
+    def extend_url(self, extention):
+        return self.url + extention
